93a4ee1eb5c6074b6ec09cd5dc5b7ef0adf14047
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-10 14:16:41 -0700

    Experiments with functions

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Follow along with the examples here
+/* function doNothing() {}
+
+function sayHello()
+{
+    console.log("Hello!");
+}
+
+sayHello();
+
+function sayHelloToGuadalupe() 
+{
+    console.log("Hello, Guadalupe!");
+}
+
+function sayHelloToLiz() 
+{
+    console.log("Hello, Liz!");
+}
+
+function sayHelloToSamip() 
+{
+    console.log("Hello, Samip!");
+}
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+function doSomething(thing)
+{
+    console.log(thing)
+}
+
+doSomething("anything");
+*/
+
+/* function sayHello(firstName)
+{
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHello("Sarah");
+sayHello("Lewis");
+*/
+
+function say(greeting, firstname)
+{
+    return `${greeting}, ${firstname}!`;
+}
+
+console.log(say("Hello","Liz"))
+
+
